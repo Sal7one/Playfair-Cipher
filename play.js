0169e5a4da0478f07b0a87dd62b58129af116629
Saleh Alanazi <Alanazi.saleh.77@gmail.com>
2023-02-05 01:15:26 +0300

    encryption almost done

--- play.js
+++ play.js
@@ -30,6 +30,7 @@ let plainText = "Some Text a"
 let cipherText = ""
 let key = "Ahmedo"
 let grid = null
+let placeHolderLetter = "x"
 let gridElm = document.querySelector("#grid");
 let keyElm = document.querySelector("#key");
 let plainTextElm = document.querySelector("#plain");
@@ -71,18 +72,61 @@ function buildLetterRow(arrayOfLetters) {
   </div>`
 }
 
+
+// Encrypt 
+// Same row -> right 
+// Same column -> down
+// Digonale -> switch
 function encrypt(plainText, grid){
 
-let plainTextWithoutSpace = plainText.replaceAll(" ", "")
+let plainTextWithoutSpace = plainText.replaceAll(" ", "").toLowerCase()
 var chunks = [];
 for (var i = 0, charsLength = plainTextWithoutSpace.length; i < charsLength; i += 2) {
     chunks.push(plainTextWithoutSpace.substring(i, i + 2));
 }
 
-let lastChunck = chunks[chunks.length-1];
 
-console.log(plainTextWithoutSpace)
+// If Last element is one letter add X to it
+let lastChunck = chunks[chunks.length-1];
+if(lastChunck.length == 1){
+    chunks[chunks.length-1] = lastChunck + "x"
+}
+
+console.log(grid)
 console.log(chunks)
+chunks.forEach(letterCombo => {
+    let firstLetter = letterCombo[0]
+    let secondLetter = letterCombo[1]
+
+    // If hello => he lx ox
+    if(firstLetter == secondLetter)
+    secondLetter = placeHolderLetter// usually "x"
+
+    let firstLetterRow = -1;
+    let firstLetterColumn = -1;
+    let secondLetterRow = -1;
+    let secondLetterColumn = -1;
+
+    for(let i =0; i< grid.length; i++){
+        let row = grid[i]
+        if(row.indexOf(firstLetter) != -1){
+            firstLetterRow = i;
+            firstLetterColumn = row.indexOf(firstLetter);
+        }
+        if(row.indexOf(secondLetter) != -1){
+            secondLetterRow = i;
+            secondLetterColumn = row.indexOf(secondLetter)
+        }
+    }
+
+    console.log(`First Letter: ${firstLetter}, Row: ${firstLetterRow} Column: ${firstLetterColumn}`)
+    console.log(`Second Letter: ${secondLetter}, Row: ${secondLetterRow} Column: ${secondLetterColumn}`)
+    if(firstLetterRow == secondLetterRow){
+
+    }else if(firstLetterColumn == secondLetterColumn){
+
+    }
+});
 
 }
 
