f48ec79d3a0856e6a9be270db84b2a7c0ee8d1d5
Saleh Alanazi <Alanazi.saleh.77@gmail.com>
2023-02-05 00:32:49 +0300

    working on encryption

--- play.js
+++ play.js
@@ -1,4 +1,4 @@
-const AplphaBetLetters = [
+const ALPHABET = [
     'a',
     'b',
     'c',
@@ -26,46 +26,73 @@ const AplphaBetLetters = [
     'z',
 ]
 
+let plainText = "Some Text a"
+let cipherText = ""
+let key = "Ahmedo"
+let grid = null
+let gridElm = document.querySelector("#grid");
+let keyElm = document.querySelector("#key");
+let plainTextElm = document.querySelector("#plain");
+let cipherTextElm = document.querySelector("#cipher");
 
-function generateGrid(key, plainText){
+function generateGrid(key) {
 
- let uniqueKeyArray = new Set()
+    let uniqueKeyArray = new Set()
 
- // Build unqiue matrix
- for(let i = 0; i< key.length; i++ )
-    uniqueKeyArray.add(key[i].toLowerCase())
+    // Build unqiue matrix
+    for (let i = 0; i < key.length; i++)
+        uniqueKeyArray.add(key[i].toLowerCase())
 
-let uniqueMatrix = Array.from(new Set([...uniqueKeyArray, ...AplphaBetLetters]))
+    let uniqueMatrix = Array.from(new Set([...uniqueKeyArray, ...ALPHABET]))
 
-let splittedArray = []
-while(uniqueMatrix.length) {
-    splittedArray.push(uniqueMatrix.splice(0,5));
+    let splittedArray = []
+    while (uniqueMatrix.length) {
+        splittedArray.push(uniqueMatrix.splice(0, 5));
+    }
+
+    grid = splittedArray
+    let allRows = ""
+    // Build unqiue matrix
+    for (i = 0; i < splittedArray.length; i++) {
+        allRows += buildLetterRow(splittedArray[i])
+    }
+
+    gridElm.innerHTML = allRows
 }
 
-let allRows = ""
- // Build unqiue matrix
- for(i = 0; i < splittedArray.length; i++ ){
-    allRows+= buildLetterRow(splittedArray[i])
- }
+function buildLetterRow(arrayOfLetters) {
+    let letters = ""
+    arrayOfLetters.forEach(letter => {
+        letters += `<div class="letter" id="${letter}">${letter.toUpperCase()}</div>`
+    });
 
- console.log(splittedArray)
-
-
- document.querySelector("#output").innerHTML = allRows
-
-}
-
-function buildLetterRow(arrayOfLetters){
-
-let letters = ""
-
-arrayOfLetters.forEach(letter => {
-    letters+=`<div class="letter" id="${letter}">${letter.toUpperCase()}</div>`
-});
-
-return `<div class="row">
+    return `<div class="row">
    ${letters}
   </div>`
 }
 
-generateGrid("Saleha", "Hello")
+function encrypt(plainText, grid){
+
+let plainTextWithoutSpace = plainText.replaceAll(" ", "")
+var chunks = [];
+for (var i = 0, charsLength = plainTextWithoutSpace.length; i < charsLength; i += 2) {
+    chunks.push(plainTextWithoutSpace.substring(i, i + 2));
+}
+
+let lastChunck = chunks[chunks.length-1];
+
+console.log(plainTextWithoutSpace)
+console.log(chunks)
+
+}
+
+// Generate grid
+generateGrid(key)
+
+// Key 
+keyElm.innerHTML += " " + key
+plainTextElm.innerHTML += " " + plainText
+
+
+encrypt(plainText, grid)
+cipherTextElm.innerHTML += " " + cipherText
